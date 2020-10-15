87adb742ceec6505347f0ee30e11108d37f57b88
hmooe <0rw0325z7265y1y@ezweb.ne.jp>
2020-10-15 17:00:21 +0900

    Updates

---
+++ precache-manifest.a061f505abef8e618724c9b28372e662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9202b281567d41b8cf7e731ebcff6b76",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "4cf0b73e1298169c1b61",
+    "url": "/simple-todo/static/css/main.39eff0a4.chunk.css"
+  },
+  {
+    "revision": "48743d0ba2832778c5fa",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf0b73e1298169c1b61",
+    "url": "/simple-todo/static/js/main.498c44fe.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-	<div className="header">
-		<span>Simple TODO</span>
-	</div>
-);
-
-export default Header;
